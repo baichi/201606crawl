223b4a089109cdcaf865544109b11b8565ce0eeb
zhufengnodejs <zhufengnodejs@126.com>
2016-08-07 15:38:24 +0800

    这是一次新的提交

--- push.js
+++ push.js
@@ -3,16 +3,21 @@
 // git push origin master
 var exec = require('child_process').exec;
 var async = require('async');
-async.series([
-    function (cb) {
-        exec('git add -A', cb);
-    },
-    function (cb) {
-        exec('git commit -m"dd"', cb);
-    }, function (cb) {
-        exec('git push origin master',cb);
-    }
-], function (err, result) {
-  console.log('result',result);
-})
+//可以在控制台监听用户的输入，输入内容之后会触发此事件
+process.stdin.on('data',function(data){
+    async.series([
+        function (cb) {
+            exec('git add -A', cb);
+        },
+        function (cb) {
+            exec('git commit -m"'+data.toString()+'"', cb);
+        }, function (cb) {
+            exec('git push origin master',cb);
+        }
+    ], function (err, result) {
+        console.log('result',result);
+    })
+});
+
+
 
