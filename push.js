13b4c8c467e415249e57b36d79de9e797847d156
zhufengnodejs <zhufengnodejs@126.com>
2016-08-07 15:45:29 +0800

    提供我们需要的元素

--- push.js
+++ push.js
@@ -8,23 +8,25 @@ var job = new CronJob("1 * * * * *",function(){
     //可以在控制台监听用户的输入，输入内容之后会触发此事件
     //process.stdin.on('data',function(data){
     exec('git status',function(err,stdout,stderr){
-
+        if(stdout.indexOf('nothing to commit, working directory clean')!=-1){
+            var data = new Date.toLocaleString();
+            async.series([
+                function (cb) {
+                    exec('git add -A', cb);
+                },
+                function (cb) {
+                    exec('git commit -m"'+data.toString()+'"', cb);
+                }, function (cb) {
+                    exec('git push origin master',function(){
+                        console.log('提交成功');
+                    });
+                }
+            ], function (err, result) {
+                //console.log('result',result);
+            })
+        }
     })
-        var data = "提交";
-        async.series([
-            function (cb) {
-                exec('git add -A', cb);
-            },
-            function (cb) {
-                exec('git commit -m"'+data.toString()+'"', cb);
-            }, function (cb) {
-                exec('git push origin master',function(){
-                    console.log('提交成功');
-                });
-            }
-        ], function (err, result) {
-            //console.log('result',result);
-        })
+
     //});
 });
 
