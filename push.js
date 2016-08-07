0a798f5e8ce6deb67b00c794caf06631a03da232
zhufengnodejs <zhufengnodejs@126.com>
2016-08-07 15:31:41 +0800

    dd

---
+++ push.js
@@ -0,0 +1,18 @@
+// git add -A
+// git commit -m"dd"
+// git push origin master
+var exec = require('child_process').exec;
+var async = require('async');
+async.series([
+    function (cb) {
+        exec('git add -A', cb);
+    },
+    function (cb) {
+        exec('git commit -m"dd"', cb);
+    }, function (cb) {
+        exec('git push origin master', cb);
+    },
+], function (err, result) {
+  console.log(result);
+})
+
