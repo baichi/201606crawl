3426c6016b4013dc6e53b5a78444a802c56fa0af
zhufengnodejs <zhufengnodejs@126.com>
2016-08-07 15:42:01 +0800

    提交

--- push.js
+++ push.js
@@ -3,21 +3,25 @@
 // git push origin master
 var exec = require('child_process').exec;
 var async = require('async');
-//可以在控制台监听用户的输入，输入内容之后会触发此事件
-process.stdin.on('data',function(data){
-    async.series([
-        function (cb) {
-            exec('git add -A', cb);
-        },
-        function (cb) {
-            exec('git commit -m"'+data.toString()+'"', cb);
-        }, function (cb) {
-            exec('git push origin master',cb);
-        }
-    ], function (err, result) {
-        console.log('result',result);
-    })
+var CronJob = require('cron').CronJob;
+var job = new CronJob("1 * * * * *",function(){
+    //可以在控制台监听用户的输入，输入内容之后会触发此事件
+    //process.stdin.on('data',function(data){
+        var data = "提交";
+        async.series([
+            function (cb) {
+                exec('git add -A', cb);
+            },
+            function (cb) {
+                exec('git commit -m"'+data.toString()+'"', cb);
+            }, function (cb) {
+                exec('git push origin master',cb);
+            }
+        ], function (err, result) {
+            console.log('result',result);
+        })
+    //});
 });
 
-
+job.start();
 
